5c22b147ac1b302e631c2f1b1ddffcd679fafab6
sanssy <d.sanoussy@gmail.com>
2020-03-18 22:15:03 +0100

    add deployment script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res)=>{
-    res.send('hello world!');
+    res.send('hey world');
 });
 
 app.listen(process.env.PORT || 3000);
