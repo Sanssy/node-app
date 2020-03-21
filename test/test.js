59d7cd5b015137f4ad0bb15f300e31f1d9f85f1c
sanssy <d.sanoussy@gmail.com>
2020-03-21 01:47:15 +0100

    update script folder

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('GET /', ()=>{
-    it('should return hello world',(done)=>{
+    it('should return hey world',(done)=>{
         request(app).get('/').expect('hey world', done);
     });
 });
